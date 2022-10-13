40da68aad4e7aea2df5c0569d80fea4b3a1c878d
Matheus Lucena <matheus.supervisor@gmail.com>
2022-10-13 18:40:14 -0300

    começando o curso de Angular, parando na aula 4B

---
+++ angular/fundamentos-angular/src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-primeiro-component",
+    template: "<h2>Primeiro Component</h2>",
+    styles: ["h2  { color: red;} "]
+})
+
+export class PrimeiroComponentComponent {}
